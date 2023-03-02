cc08764c6086862bc25064efa3212e7897c941c4
paigeschn <paigekennedy803@gmail.com>
2023-03-02 15:48:33 -0600

    fetch json

---
+++ Midterm_26/index.js
@@ -0,0 +1,26 @@
+fetch('data.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error:' + err);
+    })
+
+function appendData(data){
+    let mainContainer = document.getElementById("myData");
+    for (let productName in data) {
+        let div = document.createElement("div");
+        div.innerHTML = `<br> <br> <h2> ${productName} </h2>`;
+        mainContainer.appendChild(div);
+    let mainContainer2 = document.getElementById("myProduct");
+        for (let element of data[productName]) {
+            console.log(element);
+            let div2 = document.createElement("div");
+            div2.innerHTML = `${element["productId"]} : ${element["shortDescription"]} <br>`;
+            mainContainer.appendChild(div2);
+        }
+    }
+}          
