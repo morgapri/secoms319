455dcfd4ae52bc3cdc79465c51c9d626132e5fc6
paigeschn <paigekennedy803@gmail.com>
2023-03-03 17:19:03 -0600

    Style formating

--- Midterm_26/index.js
+++ Midterm_26/index.js
@@ -10,21 +10,6 @@ fetch('data.json')
     })
 
 function appendData(data){
-    // let mainContainer = document.getElementById("restaurants");
-    // for (let restaurantName in data) {
-    //     let div = document.createElement("div");
-    //     div.innerHTML = `<br> <br> <h2> ${restaurantName} </h2>`;
-    //     mainContainer.appendChild(div);
-    //     //let mainContainer2 = document.getElementById("data");
-    //     for (let element of data[restaurantName]) {
-    //         console.log(element);
-    //         let div2 = document.createElement("div");
-    //         div2.innerHTML = `${element["id"]} : ${element["foodName"]} <br>
-    //         <img src= "${element['imageBuilding']}" alt = "${element['name']}" width = "400px" height = "250px"> <br>`;
-    //         mainContainer.appendChild(div2);
-    //     }
-    // }
-
     let mcdonalds = document.getElementById("mcdonalds");
     let subway = document.getElementById("subway");
     let culvers = document.getElementById("culvers");
