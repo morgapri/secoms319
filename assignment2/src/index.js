944233157abc1a26efd236c2e5d295226d821755
paigeschn <paigekennedy803@gmail.com>
2023-04-03 15:05:21 -0500

    added class labels

--- assignment2/src/index.js
+++ assignment2/src/index.js
@@ -12,15 +12,16 @@ root.render(
 );
 
 function DisplayItems(){
-const [ProductsCategory, setProductsCategory] = useState(Products);
-return <div>
-  {ProductsCategory.map((product, index) => (
-    <div key={index} >
-      <img alt="Product Image" src={product.image} />
-      <h3>{product.title}</h3>
-      <p>{product.price}</p>
-      <p>{product.description}</p>
-    </div>
-  ))}
-</div>
-  }
+  const [ProductsCategory, setProductsCategory] = useState(Products);
+
+  return <div>
+    {ProductsCategory.map((product, index) => (
+      <div key={index} >
+        <img alt="Product Image" src={product.image} />
+        <h3>{product.title}</h3>
+        <p class="price">{product.price}</p>
+        <p>{product.description}</p>
+      </div>
+    ))}
+  </div>
+}
