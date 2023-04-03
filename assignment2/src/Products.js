ffe72debd6e01853d3e79ef3817f373be668ca31
paigeschn <paigekennedy803@gmail.com>
2023-04-03 14:32:18 -0500

    product temp filled

--- assignment2/src/Products.js
+++ assignment2/src/Products.js
@@ -1,20 +1,44 @@
 export const Products = [
     {
         "id": 1,
+        "title": "",
+        "price": 0,
+        "description": "",
+        "image": "",
     },
     {
         "id": 2,
+        "title": "",
+        "price": 0,
+        "description": "",
+        "image": "",
     },
     {
         "id": 3,
+        "title": "",
+        "price": 0,
+        "description": "",
+        "image": "",
     },
     {
         "id": 4,
+        "title": "",
+        "price": 0,
+        "description": "",
+        "image": "",
     },
     {
         "id": 5,
+        "title": "",
+        "price": 0,
+        "description": "",
+        "image": "",
     },
     {
         "id": 6,
+        "title": "",
+        "price": 0,
+        "description": "",
+        "image": "",
     }
 ]
