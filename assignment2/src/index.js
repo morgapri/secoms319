05743cb3dcaa544b1bb4fc284054ad3cedddb205
paigeschn <paigekennedy803@gmail.com>
2023-04-03 14:55:25 -0500

    products displayed

--- assignment2/src/index.js
+++ assignment2/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {useState, useEffect} from "react";
 import './index.css';
-import './Products'
+import {Products} from './Products'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <div>
+    <DisplayItems />
+  </div>
 );
 
-
+function DisplayItems(){
+const [ProductsCategory, setProductsCategory] = useState(Products);
+return <div>
+  {ProductsCategory.map((product, index) => (
+    <div key={index} >
+      <img alt="Product Image" src={product.image} />
+      <h3>{product.title}</h3>
+      <p>{product.price}</p>
+      <p>{product.description}</p>
+    </div>
+  ))}
+</div>
+  }
