7f92b9afa799b8c80dbbcf3fce0771e77f93edcd
morgapri <morganprieskorn35@gmail.com>
2023-04-03 15:16:48 -0500

    fixed fan

--- assignment2/src/Products.js
+++ assignment2/src/Products.js
@@ -11,7 +11,7 @@ export const Products = [
         "title": "Electric Fan",
         "price": 15.99,
         "description": "This patented electric fan cools you with its spinning blades!",
-        "image": "https://ihttps://images.pexels.com/photos/3675622/pexels-photo-3675622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1mages.pexels.com/photos/7936648/pexels-photo-7936648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "image": "https://images.pexels.com/photos/1374448/pexels-photo-1374448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         "id": 3,
