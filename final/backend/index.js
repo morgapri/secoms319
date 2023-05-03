53aa032488e789e42edf757632c0ed91afc42af4
morgapri <morganprieskorn35@gmail.com>
2023-05-02 20:09:23 -0500

    made some changes

--- final/backend/index.js
+++ final/backend/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const Product = require("./dataSchema.js");
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
 mongoose.connect("mongodb://127.0.0.1:27017/reactdata",
 {
 dbName: "reactdata",
@@ -13,6 +17,16 @@ useUnifiedTopology: true,
 );
 const port = process.env.PORT || 4000;
 const host = "localhost";
+
+
+app.get("/", async (req, resp) => {
+const query = {};
+const allProducts = await Product.find(query);
+console.log(allProducts);
+resp.send(allProducts);
+});
+
+
 app.listen(port, () => {
 console.log(`App listening at http://%s:%s`, host, port);
 });
