f4c724d0194551daa0af7d72a4811beb3893e392
morgapri <morganprieskorn35@gmail.com>
2023-05-02 20:12:23 -0500

    added basic show stuff to make sure it works

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [product, setProduct] = useState([]);
+const [viewer1, setViewer1] = useState(false);
 
+const showAllItems = product.map((el) => (
+  <div key={el._id}>
+  <img src={el.image} width={30} /> <br />
+  Title: {el.title} <br />
+  Category: {el.category} <br />
+  Price: {el.price} <br />
+  Rate :{el.rating.rate} and Count:{el.rating.count} <br />
+  </div>
+  ));
+  
+
+function getAllProducts() {
+  fetch("http://localhost:4000/")
+  .then((response) => response.json())
+  .then((data) => {
+  console.log("Show Catalog of Products :");
+  console.log(data);
+  setProduct(data);
+  });
+  setViewer1(!viewer1);
+  }
+  
+
+return (
+  <div>
+  <h1>Catalog of Products</h1>
+  <button onClick={() => getAllProducts()}>Show All products</button>
+  <h1>Show all available Products.</h1>
+  <hr></hr>
+  {viewer1 && <div>Products {showAllItems}</div>}
+  <hr></hr>
+  </div>
+  )
+
+} // App end
 export default App;
