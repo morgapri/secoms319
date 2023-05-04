930228d9e596a5ddeb7a851fc9328b54e88b89ec
paigeschn <paigekennedy803@gmail.com>
2023-05-04 14:10:31 -0500

    cosmetics

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -28,12 +28,22 @@ function getAllProducts() {
 
 return (
   <div>
-  <h1>Catalog of Products</h1>
-  <button onClick={() => getAllProducts()}>Show All products</button>
-  <h1>Show all available Products.</h1>
-  <hr></hr>
-  {viewer1 && <div>Products {showAllItems}</div>}
-  <hr></hr>
+    <div id="navbar" class="container-fluid">
+        <nav class="navbar navbar-expand navbar-dark bg-fark">
+          <button id="home">Home</button>
+          <h1>Search</h1>
+          <input type="text" id="message" name="message" placeholder="id" /*onChange={(e) =>getOneProduct(e.target.value)}*/ />
+        </nav>
+    </div>
+    <hr></hr>
+    <div id="body">
+    <h1>Catalog of Products</h1>
+    <button onClick={() => getAllProducts()}>Show All products</button>
+    <h1>Show all available Products.</h1>
+    <hr></hr>
+    {viewer1 && <div>Products {showAllItems}</div>}
+    <hr></hr>
+    </div>
   </div>
   )
 
