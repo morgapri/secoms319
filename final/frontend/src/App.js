03f2c1abb8e5e7e69b5975e91a208dae8c344c9f
morgapri <morganprieskorn35@gmail.com>
2023-05-04 19:06:05 -0500

    added cart stuff, but isnt showing up

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; 
 function App() {
 const [product, setProduct] = useState([]);
 const [viewer1, setViewer1] = useState(false);
@@ -47,6 +47,95 @@ function getAllProducts() {
   }
   
 
+//stuff from assignment 2 cart
+ const [ProductsCategory, setProductsCategory] = useState(product);
+  const [query, setQuery] = useState('');
+  const[cart, setCart] = useState([]);
+  const[cartTotal, setCartTotal] = useState(0);
+  let tax = cartTotal.toFixed(2) - (cartTotal.toFixed(2) / 1.1);
+
+  useEffect(() => {
+      total();
+  }, [cart])
+
+  const addToCart = (el) => {
+    if(el.amount == 0){
+      setCart([...cart, el])
+      {el.amount++}
+    }
+    else{
+      let id;
+      for(let j = 0; j < cart.length; j++){
+        if(cart[j].id == el.id){
+          id = j;
+        }
+      }
+      let hardCopy = [...cart];
+      hardCopy[id].amount = el.amount++;
+      {el.amount++}
+      setCart(hardCopy)
+    }
+  }
+
+  const total = () => {
+      let totalVal = 0;
+      let tax = 1.1;
+      for(let i = 0; i < cart.length; i++)
+      {
+          totalVal+=cart[i].price*cart[i].amount;
+      }
+      totalVal = totalVal * tax;
+      setCartTotal(totalVal);
+  }
+
+  const removeFromCart = (el) =>
+  {
+    let id;
+    let hardCopy;
+      for(let j = 0; j < cart.length; j++){
+        if(cart[j].id == el.id){
+          id = j;
+        }
+      }
+
+    if(el.amount > 1){
+      hardCopy = [...cart];
+      hardCopy[id].amount = el.amount--;
+      setCart(hardCopy)
+    }
+    else{
+      hardCopy = [...cart];
+      hardCopy = hardCopy.filter((cartItem) => cartItem.id != el.id);
+      setCart(hardCopy);
+    }
+    if(el.amount > 0){
+      {el.amount--}
+    }
+  }
+
+  const cartItems = cart.map((el) => (
+      <div key={el.id}>
+          <img class = "img-fluid" src = {el.image} width={100}/>
+          {el.title}:
+           ${el.price}:
+          x{el.amount}
+      </div>
+  ))
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    const results = ProductsCategory.filter(eachProduct => {
+   
+    if (e.target.value === "") return ProductsCategory;
+    return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
+    });
+    setProductsCategory(results);
+  }
+
+  function clear() {
+    setProductsCategory(product);
+  }
+
 return (
   <div>
     <div id="navbar" class="container-fluid">
@@ -71,7 +160,22 @@ return (
         <hr></hr>
       </div>
       <div id="cart">
-
+        {ProductsCategory.map((product, index) => (
+      <div key={index} >
+        <img alt="Product Image" src={product.image} />
+        <h3>{product.title}</h3>
+        <p class="price">${product.price}</p>
+        <p>{product.description}</p>
+        <button type="button" onClick={() => removeFromCart(product)}>-</button>{" "}
+        <button type="button" variant="light" onClick={() => addToCart(product)}>+</button>
+        <p>Amount: {product.amount}</p>
+        <div class="gap"></div>
+        <h3>Cart</h3>
+    <div>{cartItems}</div>
+    <p>Tax (10%): ${tax.toFixed(2)}</p>
+    <p>Total: ${cartTotal.toFixed(2)}</p>
+      </div>
+    ))}
       </div>
       <div id="checkoutpage">
         {/*data for order form in index.js file in frontend, access with id form */}
